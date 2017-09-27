aad57968e6732e1dc50120dd59cb3853493557ca
ad2bg <alex.dimitrov.bg@gmail.com>
2017-09-27 20:50:20 +0300

    PossibleAnnounce method implemented in BasePlayer

--- Source/Santase.Logic/Players/BasePlayer.cs
+++ Source/Santase.Logic/Players/BasePlayer.cs
@@ -21,6 +21,44 @@
             this.cards.Add(card);
         }
 
-        public abstract PlayerAction GetTurn(PlayerTurnContext context);
+        public abstract PlayerAction GetTurn(
+            PlayerTurnContext context,
+            IPlayerActionValidator actionValidator);
+
+        protected Announce PossibleAnnounce(Card cardToBePlayed, Card trumpCard)
+        {
+            CardType cardType = cardToBePlayed.Type;
+            CardType cardTypeToSearch;
+
+            if (cardType == CardType.Queen)
+            {
+                cardTypeToSearch = CardType.King;
+            }
+            else if (cardType == CardType.King)
+            {
+                cardTypeToSearch = CardType.Queen;
+            }
+            else
+            {
+                return Announce.None;
+            }
+
+            Card cardToSearch = new Card(cardToBePlayed.Suit, cardTypeToSearch);
+
+            if (!this.cards.Contains(cardToSearch))
+            {
+                return Announce.None;
+            }
+
+            if (cardToBePlayed.Suit==trumpCard.Suit)
+            {
+                return Announce.Fourty;
+            }
+            else
+            {
+                return Announce.Twenty;
+            }
+        }
+
     }
 }
