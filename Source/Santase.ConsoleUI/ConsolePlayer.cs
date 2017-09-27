11d9458ba55d5b8f3c61b9c84d947d93e171646c
ad2bg <alex.dimitrov.bg@gmail.com>
2017-09-27 11:23:16 +0300

    Implemented players abstraction

---
+++ Source/Santase.ConsoleUI/ConsolePlayer.cs
@@ -0,0 +1,38 @@
+﻿namespace Santase.ConsoleUI
+{
+    using System;
+    using System.Threading;
+    using Santase.Logic.Cards;
+    using Santase.Logic.Players;
+
+    public class ConsolePlayer : BasePlayer
+    {
+        int row;
+        int col;
+
+        public ConsolePlayer(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public override void AddCard(Card card)
+        {
+            base.AddCard(card);
+
+            Console.SetCursorPosition(this.col, this.row);
+            foreach (var item in this.cards)
+            {
+                Console.Write("{0} ", item.ToString());
+            }
+            Thread.Sleep(150);
+        }
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            // TODO: Implement
+            //Console.ReadLine();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Santase.Logic/Players/BasePlayer.cs
@@ -0,0 +1,26 @@
+﻿namespace Santase.Logic.Players
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Santase.Logic.Cards;
+
+    public abstract class BasePlayer : IPlayer
+    {
+        protected IList<Card> cards;
+
+        protected BasePlayer()
+        {
+            this.cards = new List<Card>();
+        }
+
+        public virtual void AddCard(Card card)
+        {
+            this.cards.Add(card);
+        }
+
+        public abstract PlayerAction GetTurn(PlayerTurnContext context);
+    }
+}
